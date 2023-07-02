8cf964427cf926d1d7f0140823bb6bdd97f40aad
Caio Pires <caiogodoi_92@hotmail.com>
2023-07-01 23:00:42 -0300

    validations

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import { MongoClient } from 'mongodb'
+import Joi from 'joi'
+import dayjs from 'dayjs'
 
 const app = express()
 const port = 5000
@@ -10,7 +12,7 @@ dotenv.config()
 app.use(cors())
 app.use(express.json())
 
-const mongoClient = new MongoClient('mongodb://localhost:27017/batepapo')
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
 let db
 
 mongoClient
@@ -45,29 +47,74 @@ app.get('/participants', (req, res) => {
 app.post('/participants', (req, res) => {
   const { name } = req.body
 
-  const participant = { name }
-  db.collection('participants')
-    .insertOne(participant)
-    .then(() => {
-      return res.sendStatus(201)
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    })
+  const participantsSchema = Joi.object({
+    name: Joi.string().required().min(1)
+  })
+
+  const validationResult = participantsSchema.validate(req.body, {
+    abortEarly: false
+  })
+
+  if (validationResult.error) {
+    const errors = validation.error.details.map(detail => detail.message)
+    return res.status(422).send(errors)
+  } else {
+    db.collection('participants')
+      .findOne({ name })
+      .then(existingParticipant => {
+        if (existingParticipant) {
+          return res.sendStatus(409)
+        } else {
+          const participant = { name, lastStatus: Date.now() }
+          db.collection('participants')
+            .insertOne(participant)
+            .then(() => {
+              return res.sendStatus(201)
+            })
+            .catch(err => {
+              res.status(500).send(err.message)
+            })
+        }
+      })
+      .catch(err => {
+        res.sendStatus(500).send(err.message)
+      })
+  }
 })
 
 app.post('/messages', (req, res) => {
   const { to, text, type } = req.body
+  const from = req.headers.user
 
-  const message = { to, text, type }
-  db.collection('messages')
-    .insertOne(message)
-    .then(() => {
-      return res.sendStatus(201)
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    })
+  const message = {
+    from,
+    to,
+    text,
+    type,
+    time: dayjs().format('HH:mm:ss')
+  }
+
+  const messageSchema = Joi.object({
+    to: Joi.string().required().min(1),
+    text: Joi.string().required().min(1),
+    type: Joi.string().valid('message', 'private_message').required()
+  })
+
+  const validation = messageSchema.validate(req.body, { abortEarly: false })
+
+  if (validation.error) {
+    const errors = validation.error.details.map(detail => detail.message)
+    return res.status(422).send(errors)
+  } else {
+    db.collection('messages')
+      .insertOne(message)
+      .then(() => {
+        return res.sendStatus(201)
+      })
+      .catch(err => {
+        res.status(422).send(err.message)
+      })
+  }
 })
 
 app.listen(port, () => {
